b9a8a67ef23309fbe77c1d890d6cb5750bcfb157
Leon <lshai078@uottawa.ca>
2020-05-25 19:38:04 -0400

    All 6 questions and varieties of questions have been added

--- main.js
+++ main.js
@@ -32,6 +32,70 @@ function outputUpdate(level) {
   }
 	//document.querySelector('#agreementLevel').value = level;
 }
+function outputUpdate2(level) {
+  if (level == 1) {
+    //document.getElementById('agreementLevel').value = "level baby";
+    document.getElementById('agreementLevel2').value = "Strongly Disagree";
+    //alert("hello");
+  }
+  if (level == 2) {
+    document.getElementById('agreementLevel2').value = "Disagree";
+    //alert("hello");
+  }
+  if (level == 3) {
+    document.getElementById('agreementLevel2').value = "Slightly Disagree";
+    //alert("hello");
+  }
+  if (level == 4) {
+    document.getElementById('agreementLevel2').value = "Neutral";
+    //alert("hello");
+  }
+  if (level == 5) {
+    document.getElementById('agreementLevel2').value = "Slightly Agree";
+    //alert("hello");
+  }
+  if (level == 6) {
+    document.getElementById('agreementLevel2').value = "Agree";
+    //alert("hello");
+  }
+  if (level == 7) {
+    document.getElementById('agreementLevel2').value = "Strongly Agree";
+    //alert("hello");
+  }
+	//document.querySelector('#agreementLevel').value = level;
+}
+function outputUpdate3(level) {
+  if (level == 1) {
+    //document.getElementById('agreementLevel').value = "level baby";
+    document.getElementById('agreementLevel3').value = "Strongly Disagree";
+    //alert("hello");
+  }
+  if (level == 2) {
+    document.getElementById('agreementLevel3').value = "Disagree";
+    //alert("hello");
+  }
+  if (level == 3) {
+    document.getElementById('agreementLevel3').value = "Slightly Disagree";
+    //alert("hello");
+  }
+  if (level == 4) {
+    document.getElementById('agreementLevel3').value = "Neutral";
+    //alert("hello");
+  }
+  if (level == 5) {
+    document.getElementById('agreementLevel3').value = "Slightly Agree";
+    //alert("hello");
+  }
+  if (level == 6) {
+    document.getElementById('agreementLevel3').value = "Agree";
+    //alert("hello");
+  }
+  if (level == 7) {
+    document.getElementById('agreementLevel3').value = "Strongly Agree";
+    //alert("hello");
+  }
+	//document.querySelector('#agreementLevel').value = level;
+}
 
 function main() {
   //document.getElementById("myButton").click();
